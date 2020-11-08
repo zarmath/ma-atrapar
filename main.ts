72cf58e93bc2730197073de10af125bc6ad32586
Fernando Cruz Bello <fernando.cruzbello@gmail.com>
2020-11-08 16:43:17 +0100

    cambiar personajes

--- main.ts
+++ main.ts
@@ -4,22 +4,22 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 let mySprite2: Sprite = null
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 3 3 3 3 2 4 4 4 . . . 
-    . . . . 3 3 3 2 2 2 3 3 4 . . . 
-    . . . 3 3 3 2 2 2 6 6 6 4 . . . 
-    . . 3 3 3 2 2 6 6 a a a 4 . . . 
-    . . 3 3 3 2 6 6 a a a a 3 . . . 
-    . . . 3 3 6 6 a a 4 3 3 . . . . 
-    . . . . 3 3 3 3 3 3 3 . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 80, 80)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
@@ -28,30 +28,22 @@ info.setScore(0)
 info.startCountdown(10)
 game.onUpdateInterval(2000, function () {
     mySprite2 = sprites.create(img`
-        ....ffffff..............
-        ..ffeeeef2f.............
-        .ffeeeef222f............
-        .feeeffeeeef............
-        .ffffee2222ef...........
-        .fe222ffffe2f...........
-        fffffffeeefff...........
-        ffe44ebf44eef...........
-        fee4d41fddef............
-        .feee4ddddf.............
-        ..fdde444ef.............
-        ..fdde22ccc.............
-        ...eef22cdc.............
-        ...f4444cddc............
-        ....fffffcddc...........
-        .....fff..cddc..........
-        ...........cdc..........
-        ............cc..........
-        ........................
-        ........................
-        ........................
-        ........................
-        ........................
-        ........................
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+        b d d d b b d 5 5 4 4 4 4 4 b . 
+        b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+        c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+        c b d c d 5 5 b 5 5 5 5 5 5 b . 
+        . c d d c c b d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     mySprite2.setPosition(randint(0, 160), randint(0, 120))
     timer.after(1500, function () {
